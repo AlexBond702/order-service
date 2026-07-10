215442639c73b8063a8d59939076208cb1fac5e4
AlexBond702 <bond.alex.702@gmail.com>
2026-07-10 18:58:46 +0500

    TASK-4006: Внес правки

--- internal/app/processor/http/api_generic.go
+++ internal/app/processor/http/api_generic.go
@@ -19,7 +19,8 @@ func GenericRegMetrics(r *gin.Engine) {
 }
 
 func GenericRegPprof(r *gin.Engine) {
-	r.GET("debug/pprof/", gin.WrapF(pprof.Index))
+	r.GET("/debug/pprof/", gin.WrapF(pprof.Index))
+	r.GET("/debug/pprof", gin.WrapF(pprofIndex))
 	r.GET("/debug/pprof/cmdline", gin.WrapF(pprof.Cmdline))
 	r.GET("/debug/pprof/profile", gin.WrapF(pprof.Profile))
 	r.POST("/debug/pprof/profile", gin.WrapF(pprof.Profile))
@@ -32,4 +33,8 @@ func GenericRegPprof(r *gin.Engine) {
 	r.GET("/debug/pprof/heap", gin.WrapH(pprof.Handler("heap")))
 	r.GET("/debug/pprof/mutex", gin.WrapH(pprof.Handler("mutex")))
 	r.GET("/debug/pprof/threadcreate", gin.WrapH(pprof.Handler("threadcreate")))
+}
+
+func pprofIndex(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/debug/pprof/", http.StatusMovedPermanently)
 }
